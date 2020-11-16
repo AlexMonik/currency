db2d69bf2789a6bbfcb36b41de9d84049a808a08
AlexMonik <64943432+AlexMonik@users.noreply.github.com>
2020-11-17 00:03:02 +0800

    changes according to heroku reqs

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,18 @@
 import psycopg2
+import urllib.parse as urlparse
+import os
 
-connection = psycopg2.connect("dbname=bot user=postgres password=3233972652")
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
+
+connection = psycopg2.connect(dbname=dbname,
+                              user=user,
+                              password=password,
+                              host=host,
+                              port=port
+                              )
 cursor = connection.cursor()
-
